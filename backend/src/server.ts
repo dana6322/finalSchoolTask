eb7f8fd0427ff2d4a0b30202dd04b45dbebebd5e
Dana Uzlaner <ronidaor3@gmail.com>
2026-03-18 02:38:33 +0200

    deployment6

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
 import initApp from "./index";
 
-const port = process.env.PORT || 3000;
+const port = parseInt(process.env.PORT || "3000", 10);
 
 initApp().then((app) => {
   console.log("after initApp");
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`Example app listening at http://0.0.0.0:${port}`);
   });
 });
